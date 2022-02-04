17b7236e3b2d4a6032306c50c3a7a7b9dc2e297d
bellrise <bellrise.dev@gmail.com>
2022-02-04 21:06:44 +0100

    Support bash completions

---
+++ target/bashauto.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Bash autocomplete for the `build` tool.
+
+_build()
+{
+    local cur prev opts
+    opts='-e -f -h -s -t -v --help'
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    [ "$(echo $cur | cut -c -1)" = "-" ] && {
+        COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+        return
+    }
+
+    [ "$prev" = "-f" ] && {
+        compopt -o filenames
+        COMPREPLY=($(compgen -f -- "${cur}"))
+    } || {
+        # Autocomplete the target names from the buildfile, apart from the
+        # already selected ones.
+        [ -f buildfile ] && opts="$(cat buildfile | grep '^@[^_]' | \
+            sed 's/^@\(\w*\).*/\1/')"
+        for word in ${COMP_WORDS[@]}; do
+            [ "$word" = "$cur" ] && continue
+            [ "$(echo $word | cut -c -1)" = "-" ] && continue
+            opts=$(echo $opts | sed "s/$word//g")
+        done
+        COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+    }
+}
+
+complete -F _build build
--- target/install.sh
+++ target/install.sh
@@ -5,8 +5,13 @@
 [ "$(basename $PWD)" = "target" ] && echo 'Must be ran from the project root' \
     && exit 1
 
+[ ! -f ./target/build ] && {
+    echo 'run a regular `build` first'
+    exit 1
+}
+
 mkdir -p /usr/share/man/man1
-mv -f ./target/build /usr/bin/build
+cp -f ./target/build /usr/bin/build
 [ $? = 0 ] || {
     echo 'build install must be run as root'
     exit 1
@@ -15,3 +20,8 @@ cp -f ./build.1 /usr/share/man/man1/build.1
 
 echo 'Installed to /usr/bin/build'
 echo 'Installed manpage build(1)'
+
+[ "$(basename $SHELL)" = "bash" ] && {
+    cp -f ./target/bashauto.sh /etc/bash_completion.d/build
+    echo 'Installed bash autocompletions'
+}
