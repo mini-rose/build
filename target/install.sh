9ee9c09796dd2b0844b8a79d53d4ab625523d5d3
fenze <contact@fenze.dev>
2022-02-26 21:35:01 +0100

    Cleaned install script

--- target/install.sh
+++ target/install.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
-
+# Author: bellrise <bellrise@minirose.dev>, fenze <contact@fenze.dev>
 # Install the build tool
 
-[ "$(basename $PWD)" = "target" ] && echo 'Must be ran from the project root' \
-    && exit 1
-
-[ ! -f ./target/build ] && {
-    echo 'run a regular `build` first'
+[ $(basename $PWD) = target ] && {
+		echo 'Must be ran from the project root'
     exit 1
 }
 
-mkdir -p /usr/share/man/man1
-cp -f ./target/build /usr/bin/build
 [ $? = 0 ] || {
     echo 'build install must be run as root'
     exit 1
 }
-cp -f ./build.1 /usr/share/man/man1/build.1
 
-echo 'Installed to /usr/bin/build'
-echo 'Installed manpage build(1)'
-
-[ "$(basename $SHELL)" = "bash" ] && {
-    cp -f ./target/bashauto.sh /etc/bash_completion.d/build
-    echo 'Installed bash autocompletions'
+[ -f target/build ] && {
+	mkdir -p /usr/share/man/man1
+	cp -f ./target/build /usr/bin/build
+	cp -f ./build.1 /usr/share/man/man1/build.1
+	cp -f ./target/bashauto.sh /etc/bash_completion.d/build
+	printf 'Installation complete. See 'man build' for more informations.'
+} || {
+    echo 'run a regular `build` first'
+    exit 1
 }
