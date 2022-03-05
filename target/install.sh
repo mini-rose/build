bb8a2aff989ab86b8047d4094182202bf2f7772e
bellrise <bellrise.dev@gmail.com>
2022-03-05 16:06:29 +0100

    fix install script
    Change my email (bellrise) to an actual working one, and fix the root
    check.

--- target/install.sh
+++ target/install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-# Author: bellrise <bellrise@minirose.dev>, fenze <contact@fenze.dev>
+# Author: bellrise <contact@bellrise.net>, fenze <contact@fenze.dev>
 # Install the build tool
 
 [ $(basename $PWD) = target ] && {
-		echo 'Must be ran from the project root'
+    echo 'Must be ran from the project root'
     exit 1
 }
 
-[ $? = 0 ] || {
+[ $(id -u) = 0 ] || {
     echo 'build install must be run as root'
     exit 1
 }
 
 [ -f target/build ] && {
-	mkdir -p /usr/share/man/man1
-	cp -f ./target/build /usr/bin/build
-	cp -f ./build.1 /usr/share/man/man1/build.1
-	cp -f ./target/bashauto.sh /etc/bash_completion.d/build
-	printf 'Installation complete. See 'man build' for more informations.'
+    mkdir -p /usr/share/man/man1
+    cp -f ./target/build /usr/bin/build
+    cp -f ./build.1 /usr/share/man/man1/build.1
+    cp -f ./target/bashauto.sh /etc/bash_completion.d/build
+    echo 'Installation complete. See `man build` for more information.'
 } || {
     echo 'run a regular `build` first'
     exit 1
